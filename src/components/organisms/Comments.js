3399ec780b064e8f153723325767d3508e1ca3ca
Jerry Zhang <jerryzhang101@yahoo.com>
2022-06-16 16:40:01 -0400

    Added he to comments

--- src/components/organisms/Comments.js
+++ src/components/organisms/Comments.js
@@ -7,8 +7,18 @@ import CommentCard from '../molecules/CommentCard';
 import CommentForm from '../molecules/CommentForm';
 
 
+// Importing node modules
+import he from 'he';
+
+
 // Component that is being exported
 const Comments = (props) => {
+
+    let comments = props.comments;
+    comments.forEach(element => {
+        element.name = he.decode(element.name);
+        element.message = he.decode(element.message);   
+    });
 
     return(
         <section className='comments'>
